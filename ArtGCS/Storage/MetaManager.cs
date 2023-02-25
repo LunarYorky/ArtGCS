ecb0e6c64057fc64d9136e181a98d4b0184d4b14
LunarYorky <lunaryorky@gmail.com>
2023-02-25 13:05:14 +0400

    Storage managers added. Base system. Can create and init storage.

--- ArtGCS/Storage/MetaManager.cs
+++ ArtGCS/Storage/MetaManager.cs
@@ -1,5 +1,58 @@
-﻿namespace ArtGCS.Serializable;
+﻿using System.Text.Json;
+using ArtGCS.Serializable.Meta;
+using static ArtGCS.Storage.ConstantsManager;
+
+namespace ArtGCS.Storage;
 
 public class MetaManager
 {
+    private readonly string _workDirectory;
+    private readonly string _infoPath;
+
+    private StorageManager _storageManager;
+    public Info Info { get; private set; } = null!; // init in TryReadInfo()
+
+    private MetaManager(string workDirectory, string infoPath, StorageManager storageManager)
+    {
+        _workDirectory = workDirectory;
+        _infoPath = infoPath;
+        _storageManager = storageManager;
+    }
+
+    public MetaManager(string workDirectory, StorageManager storageManager, Info info)
+    {
+        _workDirectory = workDirectory;
+        var infoLocalPath = Path.Combine(InfoLocalPath);
+        _infoPath = Path.Combine(workDirectory, infoLocalPath);
+        _storageManager = storageManager;
+        Info = info;
+
+        _storageManager.SerializeAndSave(1, info);
+    }
+
+    public static bool TryReadSystemFiles(string workDirectory, StorageManager storageManager,
+        out MetaManager? newMetaManager)
+    {
+        var infoPath = Path.Combine(workDirectory, InfoPath);
+        if (File.Exists(infoPath))
+        {
+            newMetaManager = new MetaManager(workDirectory, infoPath, storageManager);
+            if (newMetaManager.TryReadInfo())
+                return true;
+        }
+
+        newMetaManager = null;
+        return false;
+    }
+
+    private bool TryReadInfo()
+    {
+        if (!File.Exists(_infoPath)) return false;
+        using var stream = File.Open(_infoPath, FileMode.Open);
+        if (JsonSerializer.Deserialize(stream, typeof(Info)) is not Info info)
+            return false;
+
+        Info = info;
+        return true;
+    }
 }
--- ArtGCS/Storage/StorageManager.cs
+++ ArtGCS/Storage/StorageManager.cs
@@ -1,12 +1,164 @@
-﻿using ArtGCS.Enums;
-using ArtGCS.Parsers;
-using Newtonsoft.Json;
+﻿using System.Text.Json;
+using ArtGCS.Serializable.Meta;
+using static ArtGCS.Storage.ConstantsManager;
 
 namespace ArtGCS.Storage;
 
 public class StorageManager
 {
-    public static void CreateUser(string userName, string path)
+    public string WorkDirectory { get; }
+
+    private MetaManager _metaManager = null!; // init in CreateRepository()
+    private FileManager _fileManager = null!; // init in CreateRepository()
+
+    private StorageManager(string workDirectory)
     {
+        WorkDirectory = workDirectory;
+    }
+
+    public static bool TryReadSystemFiles(string workDirectory, out StorageManager? storageManager)
+    {
+        storageManager = new StorageManager(workDirectory);
+
+        if (FileManager.TryReadSystemFiles(workDirectory, storageManager, out var fileManager) &&
+            MetaManager.TryReadSystemFiles(workDirectory, storageManager, out var metaManager))
+        {
+            storageManager._metaManager = metaManager!;
+            storageManager._fileManager = fileManager!;
+            if (storageManager.Init())
+                return true;
+        }
+
+        storageManager = null;
+        return false;
+    }
+
+    private bool Init()
+    {
+        var files = new HashSet<string>(Directory.GetFiles(WorkDirectory));
+
+        AddFiles(WorkDirectory);
+
+        void AddFiles(string root)
+        {
+            foreach (var directory in Directory.GetDirectories(root))
+            {
+                foreach (var file in Directory.GetFiles(directory))
+                {
+                    files.Add(file);
+                }
+
+                foreach (var dir in Directory.GetDirectories(directory))
+                {
+                    AddFiles(dir);
+                }
+            }
+        }
+        
+        foreach (var fileMetaInfo in _fileManager.FilesMap.Files)
+        {
+            if (files.Remove(Path.Combine(WorkDirectory, Path.Combine(fileMetaInfo.Value.LocalPath))))
+                continue;
+
+            throw new Exception("Corrupted meta files"); //TODO
+            return false;
+        }
+
+        if (files.Count > 0)
+        {
+            
+            throw new Exception("Unregistered files"); // TODO
+        }
+
+        return true;
+    }
+
+    public static StorageManager CreateRepository(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(Path.Combine(path, UsersDirectory));
+        }
+        else if (Directory.GetFiles(path).Length > 0 || Directory.GetDirectories(path).Length > 0)
+        {
+            throw new Exception();
+        }
+
+        var time = Time.GetCurrentDateTime();
+        var info = Info.CreateDefault(time);
+        var filesMap = FilesMap.CreateDefault(time);
+
+        var storageManager = new StorageManager(path);
+
+        storageManager._fileManager =
+            new FileManager(path, storageManager, filesMap); // FileManager needs to be initialized first
+        storageManager._metaManager = new MetaManager(path, storageManager, info);
+
+        storageManager.SerializeAndSave(0, storageManager._fileManager.FilesMap);
+
+        return storageManager;
+    }
+
+    public void SerializeAndSave<T>(int id, T source)
+    {
+        if (!_fileManager.FilesMap.Files.TryGetValue(id, out var fileMetaInfo))
+            throw new Exception();
+
+        var path = Path.Combine(fileMetaInfo.LocalPath);
+        path = Path.Combine(WorkDirectory, path);
+
+
+        Console.WriteLine(path);
+        SaveFile(id, JsonSerializer.Serialize(source));
+        _fileManager.RegHash(id);
+    }
+
+    public int SerializeAndSave<T>(string[] path, T source)
+    {
+        var id = _fileManager.NewItem(path);
+        Console.WriteLine(id);
+        _fileManager.FilesMap.Files[id].LocalPath = path;
+
+        SaveFile(id, JsonSerializer.Serialize(source));
+
+        _fileManager.RegHash(id);
+        return id;
+    }
+
+    private void SaveFile(int id, string value)
+    {
+        if (!_fileManager.FilesMap.Files.TryGetValue(id, out var fileMetaInfo))
+            throw new Exception();
+
+        var path = Path.Combine(fileMetaInfo.LocalPath);
+        path = Path.Combine(WorkDirectory, path);
+
+        fileMetaInfo.LastSaveTime = Time.GetCurrentDateTime();
+
+        if (File.Exists(path))
+        {
+            File.Copy(path, path + ".bak", true);
+            File.WriteAllText(path, value);
+            File.Delete(path + ".bak");
+        }
+        else
+        {
+            File.WriteAllText(path, value);
+        }
+
+        if (id != 0)
+            _fileManager.RegHash(id);
     }
 }
+
+
+// private readonly JsonSerializer _serializer = JsonSerializer.CreateDefault(new JsonSerializerSettings()
+// {
+//     MissingMemberHandling = MissingMemberHandling.Error,
+//     Error = delegate(object? sender, ErrorEventArgs args)
+//     {
+//         args.ErrorContext.Handled = true;
+//         var currentError = args.ErrorContext.Member;
+//         Console.WriteLine(currentError?.GetType());
+//     }
+// });
