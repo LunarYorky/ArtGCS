16a07366b1bb5b7a89837179be3d64fe8fc571f8
LunarYorky <lunaryorky@gmail.com>
2023-02-25 11:09:30 +0400

    Comment getting time from web.

--- ArtGCS/Time.cs
+++ ArtGCS/Time.cs
@@ -7,14 +7,15 @@ public static class Time
 {
     public static DateTime GetCurrentDateTime()
     {
-        var client = new TcpClient("time.nist.gov", 13);
-        using var streamReader = new StreamReader(client.GetStream());
-        var response = streamReader.ReadToEnd();
-        if (response.Length < 1)
-            return DateTime.UtcNow;
-
-        var utcDateTimeString = response.Substring(7, 17);
-
-        return DateTime.ParseExact(utcDateTimeString, "yy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+        // var client = new TcpClient("time.nist.gov", 13);
+        // using var streamReader = new StreamReader(client.GetStream());
+        // var response = streamReader.ReadToEnd();
+        // if (response.Length < 1)
+        //     return DateTime.UtcNow;
+        //
+        // var utcDateTimeString = response.Substring(7, 17);
+        //
+        // return DateTime.ParseExact(utcDateTimeString, "yy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+        return DateTime.UtcNow;
     }
 }
