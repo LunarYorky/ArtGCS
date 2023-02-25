8bfa47a7da55383e82a4b2f38049d12aaa5194cf
LunarYorky <lunaryorky@gmail.com>
2023-02-25 13:00:46 +0400

    ConstantsManager

---
+++ ArtGCS/Storage/ConstantsManager.cs
@@ -0,0 +1,19 @@
+﻿namespace ArtGCS.Storage;
+
+public static class ConstantsManager
+{
+    public static void Reload(string path)
+    {
+    }
+
+
+    //Path
+    public static string MetaFilesDirectory { get; } = ".ArtGCS";
+    public static string UsersDirectory => Path.Combine(MetaFilesDirectory, "users");
+
+    public static string InfoPath => Path.Combine(MetaFilesDirectory, "info.txt");
+    public static string FileMapPath => Path.Combine(MetaFilesDirectory, "map.txt");
+
+    public static string[] InfoLocalPath => new[] { MetaFilesDirectory, "info.txt" };
+    public static string[] FileMapLocalPath => new[] { MetaFilesDirectory, "map.txt" };
+}
