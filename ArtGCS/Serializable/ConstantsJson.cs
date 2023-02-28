435f2d8207fb778064a3f16b8fdbae4075f07d7b
LunarYorky <lunaryorky@gmail.com>
2023-02-28 15:06:14 +0400

    Constants refactor

---
+++ ArtGCS/Serializable/ConstantsJson.cs
@@ -0,0 +1,9 @@
+﻿namespace ArtGCS.Serializable;
+
+public class ConstantsJson
+{
+    public string MetaFilesDirectory { get; init; }
+    public string DBName { get; init; }
+    public string UsersDirectoryName { get; init; }
+    public string SQLFilesDirectory { get; init; }
+}
