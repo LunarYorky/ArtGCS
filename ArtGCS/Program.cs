e77a9c7e0abb5e9acbe5ba58277b7766cbd131fb
LunarYorky <lunaryorky@gmail.com>
2023-02-28 15:03:08 +0400

    clear

--- ArtGCS/Program.cs
+++ ArtGCS/Program.cs
@@ -1,10 +1,4 @@
-﻿using HtmlAgilityPack;
+﻿using ArtGCS;
 
 if (args.Length < 1)
     return;
-
-// var doc = new HtmlDocument();
-// doc.LoadHtml(File.ReadAllText("RS.txt"));
-//
-// var node = doc.DocumentNode.SelectSingleNode(
-//     ".//div[@id='columnpage']/div[@class='submission-content']/section");
--- ArtGCS/Serializable/Meta/UserMetaInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using ArtGCS.Enums;
-using ArtGCS.Serializable.Meta.Sub;
-
-namespace ArtGCS.Serializable.Meta;
-
-public class UserMetaInfo
-{
-    public Dictionary<Resource, Profile> Profiles { get; set; }
-}
