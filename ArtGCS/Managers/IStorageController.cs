800ca6fe82183e3708c3c5975f39fed49a25161d
LunarYorky <lunaryorky@gmail.com>
2023-03-11 13:38:15 +0400

    refactor

---
+++ ArtGCS/Managers/IStorageController.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http.Headers;
+using ArtGCS.DAL.Entities;
+
+namespace ArtGCS.Managers;
+
+public interface IStorageController
+{
+    public string WorkDirectory { get; }
+    Task<(FileMetaInfo, HttpHeaders)> CheckOrSaveFileAsync(string localDirectoryName, Uri uri);
+    void ValidateFiles(Dictionary<string, byte[]> filesHashes);
+}
