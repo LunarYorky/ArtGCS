039e1f711dfdf35d56f0393f0a9aad9c4485111e
LunarYorky <lunaryorky@gmail.com>
2023-03-11 13:34:15 +0400

    DB contexts and auditor

---
+++ ArtGCS/DAL/ChangesAuditContext.cs
@@ -0,0 +1,20 @@
+﻿using ArtGCS.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtGCS.DAL;
+
+public sealed class ChangesAuditContext : DbContext
+{
+    public DbSet<ChangeInfo> Changes { get; set; } = null!;
+    private string DbPath { get; }
+
+    public ChangesAuditContext(string dbPath)
+    {
+        DbPath = dbPath;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options
+            .UseSqlite($"Data Source={DbPath}");
+}
